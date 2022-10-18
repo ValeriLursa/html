e5c4186375148a62d7cf9059da5f9811b2cec9a9
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-18 17:14:44 +0300

    Добавление демонстрации сетки с созранением рисунка пользователя

--- odin/Etch-a-sketch/etchASketch.js
+++ odin/Etch-a-sketch/etchASketch.js
@@ -1,13 +1,14 @@
 const panelWidth = 600;
-const borderSize = 2;
+const borderSize = 0;
 const defaultTableSize = 16;
 const defaultColorPanel = 'darkgrey';
 
-const panelDiv = document.querySelector('#panel');
+let panelDiv = document.querySelector('#panel');
 
 renderingTable(defaultTableSize);
+addClickToGridButton();
 
-const promptButton = document.querySelector('button');
+const promptButton = document.querySelector('#promptButton');
 promptButton.addEventListener('click', () => {
     const messageToPlayer = 'Enter window size from 1 to 50';
     let answerFromPlayer = prompt(messageToPlayer);
@@ -52,7 +53,7 @@ function renderingTable(answerFromPlayer) {
     }
 }
 
-function addClickToDivTable(divTable){
+function addClickToDivTable(divTable) {
     divTable.addEventListener('click', () => {
         let stringDivTableBackColor = String(divTable.style.backgroundColor);
         let paintColor = "rgb(0, 0, 0)";
@@ -62,4 +63,31 @@ function addClickToDivTable(divTable){
         }
         divTable.style.backgroundColor = paintColor;
     })
+}
+
+function addClickToGridButton() {
+    let gridButton = document.querySelector("#gridButton");
+    gridButton.addEventListener('click', () => {
+        let messageButton = gridButton.textContent;
+        if (messageButton == 'Remove grid') {
+            showGrid(0, 'square')
+            gridButton.textContent = 'Show grid';
+            return;
+        }
+        showGrid(2, 'border');
+        gridButton.textContent = 'Remove grid';
+    })
+}
+
+function showGrid(borderSize, className) {
+    let divTable;
+    if (className == 'border') divTable = document.querySelectorAll('.square');
+    else divTable = document.querySelectorAll('.border');
+    let divTableSize = Number(divTable[0].style.width.slice(0, divTable[0].style.width.indexOf('px', 0)));
+    let panelDivSize = (divTableSize + borderSize) * defaultTableSize;
+    panelDiv.style.width = String(panelDivSize) + 'px';
+    panelDiv.style.height = String(panelDivSize) + 'px';
+    divTable.forEach((divTableElem) => {
+        divTableElem.setAttribute('class', className);
+    });
 }
