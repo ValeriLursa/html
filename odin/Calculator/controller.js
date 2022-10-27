3eaf9c46de9a828b698fb8530d549b8ef582f727
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-27 12:41:20 +0300

    Переписывание алгоритма, который выполняется при нажатии на кнопку оператора. Удаление алгоритма, который должен выполняться при нажатии на кнопку равно

--- odin/Calculator/controller.js
+++ odin/Calculator/controller.js
@@ -1,70 +1,68 @@
+let calculator = new Calculator();
 const numElems = document.querySelectorAll(".num");
 const resultElem = document.querySelector("#result");
-const operator = ["+", '-', '*', "/", '=', "Clear"];
 let numberString = '';
-let firstNumber = 0;
-let secondNumber = 0;
-let operateValue = '';
 let resultMessage = '';
+
+function resultTextContent(message) {
+    resultElem.textContent = message;
+}
 
 numElems.forEach((elem) => {
     elem.addEventListener('click', () => {
         let elemText = elem.textContent;
         numberString += elemText;
         resultMessage += elemText;
-        console.log(operateValue, firstNumber, secondNumber);
-        resultElem.textContent = resultMessage;
+        resultTextContent(resultMessage);
+        console.log('numberString = ' + numberString, 'resultMessage = ' + resultMessage);
     })
 })
 
 const clearElem = document.querySelector(".clear");
 clearElem.addEventListener('click', () => {
-    resultElem.textContent = '';
-    firstNumber = 0;
-    secondNumber = 0;
-    operateValue = '';
-    checkResultButtonClick = false;
-    result = 0;
+    resultTextContent('')
     numberString = '';
     resultMessage = '';
+    countClickOperate = 0;
+    console.log('Click Clear', numberString + resultMessage);
 })
-
-let checkResultButtonClick = false;
-let checkOperateButton = false;
-let result;
 
 const resultButton = document.querySelector(".result");
 resultButton.addEventListener("click", () => {
-    if (checkResultButtonClick) {
-        resultElem.textContent = firstNumber + ' = ' + firstNumber;
-        console.log("click double click =", firstNumber);
-        return;
-    }
-    if (!checkOperateButton){
-        firstNumber = Number(numberString);
-        resultElem.textContent = firstNumber + ' = ' + firstNumber;
-        console.log("click = without operator", firstNumber);
-        return;
-    }
-    checkResultButtonClick = true;
-    secondNumber = Number(numberString);
-    numberString = '';
-    firstNumber = operate(operateValue, firstNumber, secondNumber);
-    resultMessage += ' = ' + firstNumber;
-    resultElem.textContent = resultMessage;
-    console.log("click =");
+
 })
+
+let countClickOperate = 0;
 
 const operateButton = document.querySelectorAll(".operate");
 operateButton.forEach((elem) => {
     elem.addEventListener('click', () => {
-        checkOperateButton = true;
-        checkResultButtonClick = false;
-        firstNumber = Number(numberString);
-        operateValue = elem.textContent;
+        countClickOperate++;
+        resultMessage += ' ' + elem.textContent + ' ';
+        resultTextContent(resultMessage);
+        if (!checkNumberString()) {
+            console.log("Нет первого аргумента");
+            alert("I don't have first number");
+            clearElem.click();
+            return;
+        }
+        if (countClickOperate > 1) {
+            calculator.secondNumber = Number(numberString);
+            numberString = '';
+            let result = calculator.operate(elem.textContent);
+            calculator.firstNumber = result;
+            calculator.secondNumber = 0;
+            resultMessage = result + ' ' + elem.textContent + ' ';
+            resultTextContent(resultMessage);
+            console.log('countClickOperate = '+ countClickOperate, 'result = '+ result, 'resultMessage = ' + resultMessage);
+            return;
+        }
+        calculator.firstNumber = Number(numberString);
         numberString = '';
-        resultMessage += ' ' + operateValue + ' ';
-        resultElem.textContent = resultMessage;
-        console.log("click" + operateValue);
+        console.log('click' + elem.textContent, 'firstNumber = ' + calculator.firstNumber, 'countClickOperate = ' + countClickOperate);
     })
 })
+
+function checkNumberString() {
+    if (numberString !== '') return true; else return false;
+}
