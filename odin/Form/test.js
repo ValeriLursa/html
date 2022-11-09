561002f7051e2e867f986872306b0f5b8f8b07ca
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-11-09 16:37:18 +0300

    Изменение автоматизированного тестирования формы, добалвение проверки пароля

--- odin/Form/test.js
+++ odin/Form/test.js
@@ -1,40 +1,65 @@
 array = [
-    [['Mi-ke', 'invalid'], ['Mike&', 'invalid'], ['M3ike', 'invalid'], ['mikE', 'invalid'], ['Mike', 'valid']],
-    [['mail', 'invalid'], ['mail@', 'invalid'], ['mail@yandex', 'invalid'], ['mail@yandex.', 'invalid'], ['mail@yandex.ru', 'valid']],
-    [['1', 'invalid'], ['123456', 'invalid'], ['123456b890', 'invalid'], ['12345678901', 'invalid'], ['1234567890', 'valid']]
+    [['Mi-ke', false], ['Mike&', false], ['M3ike', false], ['mikE', false], ['Mike', true]],
+    [['mail', false], ['mail@', false], ['mail@yandex', false], ['mail@yandex.', false], ['mail@yandex.ru', true]],
+    [['1', false], ['123456', false], ['123456b890', false], ['12345678901', false], ['1234567890', true]],
+    [['1234567', false], ['qqqqqqq', false], ['1qQ%', false], ['1q2E%43w&', true]],
+    [['1qQ%', false], ['1q2E%43w&', true]]
 ]
 
-let button = document.getElementById('button');
+
 let email = document.getElementById('email');
 
-function testElem(elem, row, colEnd) {
+function testElem(elem, row, colStart, colEnd) {
+    let resultTesting = true;
     console.log(elem);
-    for (let i = 0; i < colEnd; i++) {
+    for (let i = colStart; i < colEnd; i++) {
         elem.value = array[row][i][0];
-        elem.focus();
-        elem.blur();
-        button.click();        
-        // console.log(array[row][i][1]);
-        let q = elem.classList.value.match(array[row][i][1]);
-        if (q && q.input.match(array[row][i][1]))
+        button.click();
+        let q = elem.validity.valid;
+        resultTesting = q;
+        if (q === array[row][i][1])
             console.log('test number ' + row + '.' + i + ' return true');
         else {
-            console.log('ERROR ' + q);
+            console.log('ERROR ' + array[row][i][0]);
             console.log('test number ' + row + '.' + i + ' return false');
         }
     }
-    return true;
+    return resultTesting;
 }
 
+let i = 0;
+
 function testForm() {
-    let start;
-    // let end = array[0].length;
-    let end = 1;
-    testElem(firstName, 0, end);
-    // testElem(lastName, 0, end);
-    // end = array[1].length;
-    // console.log(array[1]);
-    // testElem(email, 1, end);
-    // end = array[2].length;
-    // testElem(phone, 2, end);
+    let start = 0;
+    let end;
+    let row = 0;
+    let resultTesting = true;
+    inputArray.forEach(elem => {
+        end = array[row].length;
+        resultTesting = (resultTesting && testElem(elem, row, start, end));
+        if (elem.id != 'first_name') row++;
+    })
+    // end = array[row].length;
+    // resultTesting = (resultTesting && testElem(password_first, row++, start, end));
+    return Boolean(resultTesting);
+}
+
+function testPassword() {
+    let resultTesting = true;
+    let start = 0;
+    let end;
+    let row = 3;
+    end = array[row].length;
+    resultTesting = (resultTesting && testElem(password_first, row++, start, end));
+    end = array[row].length;
+    resultTesting = (resultTesting && testElem(password_second, row, start, end));
+    return Boolean(resultTesting);
+}
+
+function testAll(){
+    let resultTesting = true;
+    resultTesting = (resultTesting && testForm());
+    firstName.value = '';
+    resultTesting = (resultTesting && testPassword());
+    return Boolean(resultTesting);
 }
