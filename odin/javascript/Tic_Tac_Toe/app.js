421c75145a2e13fa3576acddf2082a56bcf5cfa1
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-12-01 14:30:31 +0300

    подключение новых js файлов

---
+++ odin/javascript/Tic_Tac_Toe/app.js
@@ -0,0 +1,21 @@
+console.log('app.js connect');
+
+// console.log(Gameboard.getGameBoard());
+
+// const player1 = Player('Player1', 'x');
+// const player2 = Player('Player2', 'o');
+
+// console.log(player1.getName() + ' : ' + player1.getElem());
+// console.log(player2.getName() + ' : ' + player2.getElem());
+
+// console.log(Gameboard.checkResult());
+// console.log(Gameboard.setElemByGameNoard('x', 1, 1));
+// console.log(Gameboard.setElemByGameNoard('x', 1, 0));
+// console.log(Gameboard.setElemByGameNoard('x', 1, 2));
+// console.log(Gameboard.getGameBoard());
+// console.log(Gameboard.checkResult());
+// console.log(Gameboard.setElemByGameNoard('x', 1, 1));
+// console.log(Gameboard.setElemByGameNoard('x', 0, 1));
+// console.log(Gameboard.setElemByGameNoard('x', 2, 1));
+// console.log(Gameboard.getGameBoard());
+// console.log(Gameboard.checkResult());
