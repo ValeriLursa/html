b1418b969b48946c27b300ef3dac6a4090c63306
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-12-01 16:45:53 +0300

    Добавление переменной для отслеживания хода игрока

--- odin/javascript/Tic_Tac_Toe/player.js
+++ odin/javascript/Tic_Tac_Toe/player.js
@@ -2,7 +2,12 @@ const Player = (name, elem) =>{
     const getName = () => name;
     const getElem = () => elem;
 
+    let move = false;
+
+    const getMove = () => move;
+    const setMove = (moveConst) => move = moveConst;
+
     return{
-        getName, getElem
+        getName, getElem, getMove, setMove
     }
 };
