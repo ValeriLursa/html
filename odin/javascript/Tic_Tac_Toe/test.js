2f1f735c108f0d327abc8c0757d2a5123aaa1ef3
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-12-01 16:43:28 +0300

    Добавление тестового набора

--- odin/javascript/Tic_Tac_Toe/test.js
+++ odin/javascript/Tic_Tac_Toe/test.js
@@ -1,31 +1,37 @@
 const testList = [
     {
-        1: [[[0, 0, 'x'],
-        [0, 0, 'x'],
-        [0, 0, 'x']], 'Win']
+        1: [[[1, 2, 'x'],
+        [4, 5, 'x'],
+        [7, 8, 'x']], 'x']
     },
     {
-        2: [[[0, 0, 0],
-        [0, 0, 0],
-        ['x', 'x', 'x']], 'Win']
+        2: [[[1, 2, 3],
+        [4, 5, 6],
+        ['x', 'x', 'x']], 'x']
     },
     {
-        3: [[['x', 0, 0],
-        [0, 'x', 0],
-        [0, 0, 'x']], 'Win']
+        3: [[['x', 2, 3],
+        [4, 'x', 6],
+        [7, 8, 'x']], 'x']
     },
     {
-        4: [[['x', 'x', 0],
-        [0, 0, 0],
-        [0, 0, 'x']], 'Lose']
+        4: [[[1, 2, 'x'],
+        [4, 'x', 6],
+        ['x', 8, 9]], 'x']
+    },
+    {
+        5: [[['x', 'x', 3],
+        [4, 5, 6],
+        [7, 8, 'x']], 'next']
     }
 ]
 
-let  q = 1;
+let q = 1;
 for (const prop in testList) {
     Gameboard.setGameBoard(testList[prop][q][0]);
-    if (Gameboard.checkResult() == testList[prop][q][1])
-        console.log(`Test $prop return true`);
-    else console.log(`Test $prop return false`);
+    let result = Gameboard.checkResult();
+    if (result == testList[prop][q][1])
+        console.log('Test ' + (Number(prop) + 1) + ' return true');
+    else console.log('Test ' + (Number(prop) + 1) + ' return false ', result);
     q++;
 }
